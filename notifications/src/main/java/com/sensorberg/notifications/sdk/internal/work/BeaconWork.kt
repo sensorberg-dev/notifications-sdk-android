04fede09507f6df9b7842b28c9e75750aa4f8138
Ronaldo Pace <ronaldo.pace@sensorberg.com>
2018-07-26 17:55:14 +0200

    wip for beacon worker, logic seems to be there, needs testing, LOTS of testing

--- notifications/src/main/java/com/sensorberg/notifications/sdk/internal/work/BeaconWork.kt
+++ notifications/src/main/java/com/sensorberg/notifications/sdk/internal/work/BeaconWork.kt
@@ -1,7 +1,9 @@
 package com.sensorberg.notifications.sdk.internal.work
 
 import androidx.work.Worker
+import com.sensorberg.notifications.sdk.internal.TriggerProcessor
 import com.sensorberg.notifications.sdk.internal.model.BeaconEvent
+import com.sensorberg.notifications.sdk.internal.model.Trigger
 import com.sensorberg.notifications.sdk.internal.storage.BeaconDao
 import org.koin.standalone.KoinComponent
 import org.koin.standalone.inject
@@ -9,20 +11,48 @@ import org.koin.standalone.inject
 internal class BeaconWork : Worker(), KoinComponent {
 
 	private val dao: BeaconDao by inject()
+	private val triggerProcessor: TriggerProcessor by inject()
 
 	override fun doWork(): Result {
 
 		val beaconId = getBeaconId()
 		val isVisible = dao.isBeaconVisible(beaconId)
 		val events = dao.getBeaconEvents(beaconId)
-		processData(isVisible, events)
+		processData(isVisible, events)?.let {
+			triggerProcessor.process(Trigger.Beacon.getTriggerId(it.proximityUuid, it.major, it.minor, it.type), it.type)
+		}
 
-		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+		return Worker.Result.SUCCESS
 	}
 
 	companion object {
-		fun processData(visible: Boolean, events: List<BeaconEvent>) {
+		fun processData(visible: Boolean, events: List<BeaconEvent>): BeaconEvent? {
 
+			if (events.isEmpty()) {
+				return null
+			}
+
+			val event = events[0] // used for copy
+			var isVisibleState = visible
+			events.forEach {
+
+				if (isVisibleState) {
+					if (it.type == Trigger.Type.Exit) {
+						isVisibleState = false
+					}
+				} else {
+					if (it.type == Trigger.Type.Enter) {
+						isVisibleState = true
+					}
+				}
+			}
+
+			return if (isVisibleState != visible) {
+				val eventType = if (isVisibleState) Trigger.Type.Enter else Trigger.Type.Exit
+				event.copy(type = eventType)
+			} else {
+				null
+			}
 		}
 	}
 }
